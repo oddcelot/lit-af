bd3b7e8541872f98df87f547cf58b3978e06e3a7
Stefan Kopco <hey@oddcelot.dev>
2025-02-11 13:30:27 +0100

    ci: add pages pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-/// <reference types="vitest/config" />
-
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  test: {
-    // Lit recommends using browser environment for testing
-    // https://lit.dev/docs/tools/testing/#testing-in-the-browser
-    browser: {
-      enabled: true,
-      provider: "playwright",
-      instances: [{ browser: "chromium" }],
+import { defineConfig, loadEnv } from "vite";
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.BASE_PATH,
+    test: {
+      // Lit recommends using browser environment for testing
+      // https://lit.dev/docs/tools/testing/#testing-in-the-browser
+      browser: {
+        enabled: true,
+        provider: "playwright",
+        instances: [{ browser: "chromium" }],
+      },
     },
-  },
+  };
 });
