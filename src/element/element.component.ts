56c257bb34dc1c6e918fb6e4a00a438fefb3a20d
Stefan Kopco <hey@oddcelot.dev>
2025-02-11 20:26:48 +0100

    fix: resolve build errors and correct import paths

--- src/element/element.component.ts
+++ src/element/element.component.ts
@@ -1,4 +1,5 @@
-import { LitElement, css, html } from "lit";
+// import { LitElement, css, html } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import litLogo from "../assets/lit.svg";
 import viteLogo from "/vite.svg";
